72b95f00a33234eda85dec59f33a344e4b920f2d
Rene Saarsoo <nene@triin.net>
2023-01-28 14:30:50 +0200

    Style the title bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const Title = styled.h1`
-  color: green;
+  font-size: 18px;
+  margin: 0;
+  font-family: monospace;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.div`
+  background: #efefef;
+  color: #454545;
+  line-height: 32px;
+  padding: 0 10px;
+  border-bottom: 1px solid #dddddd;
 `;
 
 export function App() {
-  return <Title>Hello world!</Title>;
+  return (
+    <TitleBar>
+      <Title>SQL Explorer</Title>
+    </TitleBar>
+  );
 }
