8e8174485f42c663a46d6d786cd52e9239ae9d84
Rene Saarsoo <nene@triin.net>
2023-01-29 12:06:25 +0200

    Remove unused React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { parse } from "sql-parser-cst";
 import styled from "styled-components";
 import { CursorContext } from "./state";
